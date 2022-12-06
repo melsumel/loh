8385aa0567fcc8fbb4bbf659c3869d374582dc0b
melsumel <melsumel1488@gmail.com>
2022-12-06 19:05:46 +0500

    a

--- loh.js
+++ loh.js
@@ -1,12 +1,42 @@
 
-function NewContainerForTask() {
-  let myContainer = document.createElement('div');
-  myContainer.setAttribute('class', 'container');
 
-  let main = document.querySelector('main');
+function MakeNewTask(element){
 
-  main.appendChild(myContainer);
+  let newTask = GetTask(element);
+
+  ClearInput(element);
+
+  // console.log(newTask)
+
+  MakeNewContainerForTask();
+
+  FillNewContainer(newTask);
+
+  document.getElementsByClassName('newContainer')[0].setAttribute('class', 'container');
 }
+
+function FillNewContainer(task) {
+  let element = document.createElement('div');
+  element.setAttribute('class', 'newMain__task');
+  let newContainer = document.querySelector('newContainer');
+  document.getElementsByClassName('newContainer')[0].appendChild(element);
+
+  element = document.createElement('input');
+  element.setAttribute('type', 'checkbox');
+  element.setAttribute('name', 'task');
+  newContainer = document.querySelector('newMain__task');
+  document.getElementsByClassName('newMain__task')[0].appendChild(element);
+
+  element = document.createElement('label');
+  element.setAttribute('for', 'scales');
+  element.innerHTML = task;
+  newContainer = document.querySelector('newMain__task');
+  document.getElementsByClassName('newMain__task')[0].appendChild(element);
+
+
+  document.getElementsByClassName('newMain__task')[0].setAttribute('class', 'main__task');
+}
+
 /* _______________________________________________________________
   <div class="container">
     <div class="main__task">
@@ -14,3 +44,21 @@ function NewContainerForTask() {
       <label for="scales">task</label>
     </div>
 __________________________________________________________________*/
+
+function GetTask(element){
+  return document.getElementsByClassName(element)[0].value;
+}
+
+function ClearInput(element){
+  document.getElementsByClassName(element)[0].value = "";
+}
+
+function MakeNewContainerForTask() {
+  let myContainer = document.createElement('div');
+  myContainer.setAttribute('class', 'newContainer');
+
+  let main = document.querySelector('main');
+
+  main.appendChild(myContainer);
+
+}
